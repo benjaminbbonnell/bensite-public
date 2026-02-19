40a70cb45ccbc6b1219c803585281263f4592c27
benjaminbbonnell <benjaminbbonnell@gmail.com>
2026-02-18 21:53:25 -0500

    Removed ensemble pivot while I make corrections.

--- tasks/pivot.py
+++ tasks/pivot.py
@@ -68,56 +68,6 @@ queries = {
         JOIN currenttemps ON forecast_data.city_id = currenttemps.city_id AND forecast_data.api_name = currenttemps.api_name
         WHERE forecast_data.forecast_epoch = (SELECT current FROM current);
     ''',
-    "ensemblepivot": '''
-    WITH current AS (
-        SELECT
-        MAX(forecast_made) AS current
-    FROM weather_forecastdata
-    ),
-    currenttemps AS (
-        SELECT
-            city_id,
-            api_name,
-            temp_f AS currenttemp,
-            precip_in AS current_precip_in,
-            precip_prob AS current_precip_prob
-        FROM weather_forecastdata
-        WHERE forecast_made = (SELECT current FROM current) AND forecast_epoch = (SELECT current FROM current)
-    ),
-    forecast_data AS (
-        SELECT
-            city,
-            city_id,
-            api_name,
-            forecast_made,
-            forecast_epoch,
-            temp_f,
-            precip_in AS forecasted_precip_in,
-            precip_prob AS forecasted_precip_prob,
-            (current - forecast_made) / 3600 AS hoursbefore
-        FROM weather_forecastdata, current
-    )
-
-    INSERT INTO weather_forecastpivot(api_name, city_name, city_id, forecast_made, forecast_epoch, currenttemp, temp_f, current_precip_in, current_precip_prob, forecasted_precip_in, forecasted_precip_prob, hoursbefore)
-
-    SELECT
-        'ensemble' AS api_name,
-        forecast_data.city AS city_name,
-        forecast_data.city_id,
-        forecast_made,
-        forecast_epoch,
-        ROUND(AVG(currenttemps.currenttemp), 2),
-        ROUND(AVG(forecast_data.temp_f), 2),
-        ROUND(AVG(currenttemps.current_precip_in), 2),
-        ROUND(AVG(currenttemps.current_precip_prob), 2),
-        ROUND(AVG(forecast_data.forecasted_precip_in), 2),
-        ROUND(AVG(forecast_data.forecasted_precip_prob), 2),
-        forecast_data.hoursbefore
-        FROM forecast_data
-        JOIN currenttemps ON forecast_data.city_id = currenttemps.city_id AND forecast_data.api_name = currenttemps.api_name
-        WHERE forecast_data.forecast_epoch = (SELECT current FROM current) AND hoursbefore < 72
-        GROUP BY forecast_data.city, forecast_data.city_id, forecast_made, forecast_epoch, forecast_data.hoursbefore;
-    ''',
     "pivotv2": '''
         WITH last_hour AS (
         SELECT MAX(time_epoch) AS last_hour_time
