2e67e7a639a171e9ae22495ecf53a0e3b2375516
benjaminbbonnell <benjaminbbonnell@gmail.com>
2026-04-06 16:40:32 -0400

    Updated typing test models.

--- mysite/typing_test/models.py
+++ mysite/typing_test/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 class WordSet(models.Model):
-    set_code = models.CharField(max_length=100)
+    set_code = models.CharField(max_length=100, primary_key=True)
     set_name = models.CharField(max_length=100)
     
     class Meta:
         verbose_name_plural = "Word Sets"
 
 class Words(models.Model):
-    word = models.CharField(max_length=100)
     word_set = models.ForeignKey(WordSet, on_delete=models.CASCADE)
+    word = models.CharField(max_length=100)
+    word_count = models.IntegerField(null=True)
 
     class Meta:
         verbose_name_plural = "Words"
--- scripts/populate_words.py
+++ scripts/populate_words.py
@@ -13,13 +13,18 @@ django.setup()
 # flake8: noqa: E402
 from typing_test.models import Words, WordSet
 
-def insertwords(word, word_set):
-    Words.objects.create(word=word, word_set=word_set)
+word_set_pk = 'english_standard'
+word_set = WordSet.objects.get(pk=word_set_pk)
 
-with open('words.txt', 'r') as file:
+Words.objects.all().delete()
+
+def insertwords(word, word_count, word_set):
+    Words.objects.create(word=word, word_count=word_count, word_set=word_set)
+
+with open('words.txt', 'r', encoding='utf-8-sig') as file:
     for line in file:
-        word = line.strip()
-        word_set = WordSet.objects.get(id=1)
-        insertwords(word, word_set)
+        word = line.strip().lower()
+        word_count = word.count(" ") + 1
+        insertwords(word, word_count, word_set)
 
 print("Script complete")
